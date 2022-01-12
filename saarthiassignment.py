8786f901cecf2075da8398244f468059144c5e10
Sangram2101 <97607840+Sangram2101@users.noreply.github.com>
2022-01-12 19:17:44 +0530

    Add files via upload

---
+++ saarthiassignment.py
@@ -0,0 +1,7 @@
+Entities_list = ["kolkata", "delhi"]
+utterance_list = ["How far is <> from <>", How is the weather in <> "]
+
+                  output = ["How far is kolkata from delhi", "How far is delhi from kolkata,"How is the
+                            weather in kolkata, "How is the weather in delhi"]
+
+Wrong_output = ["How far is kolkata from kolkata", "How far is delhi from delhi"]
